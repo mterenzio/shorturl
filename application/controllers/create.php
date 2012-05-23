ebbd54d09f078d92b6d862ae5afba3bdb1ca51f1
Matt Terenzio <mterenzio@YAR-DIG-M-09.local>
2012-05-23 16:53:59 -0400

    zone set

---
+++ application/controllers/create.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Create extends CI_Controller {
+
+	public function index()
+	{
+		echo "create";
+	}
+}
+
---
+++ application/controllers/redirect.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Redirect extends CI_Controller {
+
+	public function index()
+	{
+		echo "redirect";
+	}
+}
+
