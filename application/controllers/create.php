296eb229222bcca278ebf81483f80532cc53cb40
Matt Terenzio <mterenzio@YAR-DIG-M-09.local>
2012-05-24 15:33:55 -0400

    zone set

--- application/controllers/create.php
+++ application/controllers/create.php
@@ -4,7 +4,32 @@ class Create extends CI_Controller {
 
 	public function index()
 	{
-		echo "create";
+		$proceed = false;
+		$seconds = 60*10;
+		//echo '<h1>Testing:</h1><p>Cookie: '.$_COOKIE['token'].'<br />Timestamp: '. $_POST['ts'].'</p>';
+		if(isset($_POST['ts']) && isset($_COOKIE['token']) && $_COOKIE['token'] == md5('makemeamillionbucks'.$_POST['ts'])) $proceed = true;
+
+		if(!$proceed) { 
+		echo 'Form processing halted for suspicious activity';
+		exit;
+		}
+
+		if(((int)$_POST['ts'] + $seconds) < time()) {
+		echo 'Too much time elapsed';
+		exit;
+		}
+		if (isset($_POST['url'])) {
+			$this->load->model('shorturl');
+			$shorturl = new ShortUrl();
+			if ($surl = $shorturl->createShortUrl($_POST['url'])) {
+		    	echo $surl;
+			} else {
+		    	echo 'error creating short url. . .try again';
+			}
+		} else {
+			echo 'No long url provided.';
+			exit;			
+		}
 	}
 }
 
