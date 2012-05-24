5d041f29a9e5662eee2ad887f4c81c14c648c412
Matt Terenzio <mterenzio@YAR-DIG-M-09.local>
2012-05-24 15:58:53 -0400

    zone set

--- application/views/header.php
+++ application/views/header.php
@@ -11,7 +11,7 @@
 	<link rel="stylesheet" type="text/css" href="/css/style.css" />
 	<link rel="shortcut icon" type="image/ico" href="/favicon.ico"/>
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
- <script type="text/javascript" src="http://dev.jquery.com/view/trunk/plugins/validate/jquery.validate.js"></script>    
+ <script type="text/javascript" src="http://ajax.microsoft.com/ajax/jQuery.Validate/1.6/jQuery.Validate.min.js"></script>    
 </head>
 <body >
 <div id="container">
