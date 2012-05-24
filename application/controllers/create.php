b56e1cf599bbcbfcd71d88eaef8eacddf09c213b
Matt Terenzio <mterenzio@YAR-DIG-M-09.local>
2012-05-24 18:34:53 -0400

    zone set

--- application/controllers/create.php
+++ application/controllers/create.php
@@ -4,6 +4,16 @@ class Create extends CI_Controller {
 
 	public function index()
 	{
+		$this->load->helper(array('form', 'url'));	
+		$this->load->library('form_validation');
+		$this->form_validation->set_error_delimiters('<div class="formerror">', '</div>');
+		$this->form_validation->set_rules('longurl', 'Longurl', 'trim|required|callback__valid_url');
+		if ($this->form_validation->run() == FALSE) {
+			$this->load->vars($data);
+			$this->load->view('welcome_message', $data);
+		} else {
+			//no captch captcha			
+						
 		$proceed = false;
 		$seconds = 60*10;
 		//echo '<h1>Testing:</h1><p>Cookie: '.$_COOKIE['token'].'<br />Timestamp: '. $_POST['ts'].'</p>';
@@ -18,7 +28,7 @@ class Create extends CI_Controller {
 		echo 'Too much time elapsed';
 		exit;
 		}
-		if (isset($_POST['url'])) {
+		//process form
 			$this->load->model('shorturl');
 			$shorturl = new ShortUrl();
 			if ($surl = $shorturl->createShortUrl($_POST['url'])) {
@@ -26,10 +36,19 @@ class Create extends CI_Controller {
 			} else {
 		    	echo 'error creating short url. . .try again';
 			}
-		} else {
-			echo 'No long url provided.';
-			exit;			
-		}
 	}
+	
+	function _valid_longurl($longurl)
+	{
+		if($this->input->post('longurl'))
+		{
+			if(filter_var($longurl, FILTER_VALIDATE_URL, FILTER_FLAG_SCHEME_REQUIRED) == FALSE) {
+		    	$this->form_validation->set_message('_valid_url', 'This is not a valid url. Please include http:// if you have not.');			
+				return false;
+			}
+		}
+		return true;
+	}	
+	
 }
 
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,116 +1,42 @@
-<?php /*
-	$this->load->library('aws');
-// Instantiate the class
-$dynamodb = new AmazonDynamoDB();
-
-$response = $dynamodb->get_item(array(
-    'TableName' => 'counters',
-    'Key' => array(
-        'HashKeyElement' => array( AmazonDynamoDB::TYPE_NUMBER => '1' )
-    ),
-	'AttributesToGet' => array('count'),
-	'ConsistentRead' => 'true'
-));
-
-// Check for success...
-if ($response->isOK())
-{
-    $current_count = (string) $response->body->Item->count->{AmazonDynamoDB::TYPE_NUMBER};
-	echo $current_count;
-}
-else
-{
-    print_r($response);
-}
-$next = $current_count + 1;
-echo $next;
-$update_response = $dynamodb->update_item(array(
-    'TableName' => get_cfg_var('aws.param3'), 
-        'Key' => array(
-            'HashKeyElement' => array(
-                AmazonDynamoDB::TYPE_NUMBER => '1'
-            )
-        ),
-		'Expected' => array(
-		        'count' => array( 'Value' => array (AmazonDynamoDB::TYPE_NUMBER => "$current_count" ) )
-		),
-        'AttributeUpdates' => array(
-            'count' => array(
-                'Action' => AmazonDynamoDB::ACTION_PUT,
-                'Value' => array(
-                    AmazonDynamoDB::TYPE_NUMBER => "$next"
-                )
-            )
-        )
-));
-
-if ($update_response->isOK())
-{
-    //$current_count = (string) $response->body->Item->count->{AmazonDynamoDB::TYPE_NUMBER};
-	echo "shoulda";
-}
-else
-{
-    print_r($update_response);
-}
-
-$shorturl = "shorty";
-$longurl = "longy";
-			$put = $dynamodb->update_item(array(
-			    'TableName' => get_cfg_var('aws.param2'), 
-			        'Key' => array(
-			            'HashKeyElement' => array(
-			                AmazonDynamoDB::TYPE_STRING => "$shorturl"
-			            )
-			        ),
-					'Expected' => array(
-						'shorturl' => array(
-						            "Exists" => "false"
-					)),										
-			        'AttributeUpdates' => array(
-			            'longurl' => array(
-			                'Action' => AmazonDynamoDB::ACTION_PUT,
-			                'Value' => array(
-			                    AmazonDynamoDB::TYPE_STRING => "$longurl"
-			                )
-			            )
-			        )
-			));
-			if ($put->isOK()) {
-				echo "put";  
-			} else {
-				print_r($put);
-			}
-
-*/	?>
 <?php
-$title = "url shortener";
 $this->load->view('header');
+?>
+<h2>Create your shorturl!</h2>
+<?php
+$attributes = array('class' => 'shorturlform', 'id' => 'secure');
+if (validation_errors() != '') {
+	echo "<div class=\"formerror\">There were errors. Details below.</div>";
+}
+echo form_open_multipart('/create', $attributes);
+echo "<p class=\"warning\">You must javascript enabled to use this form!</p>";
+
+$data = array(
+              'name'        => 'longurl',
+              'id'          => 'longurl'
+            );
+echo "<div class=\"formelement\">".form_error('longurl').form_label('Enter a URL to shorten:', 'longurl')</div>";
+
+//submit button and close form
+echo "<div class=\"formelement\">".form_submit('mysubmit', 'Get My ShortURL!', 'class="submit"')."</div>";
+echo form_close();
+
 ?>
 <script type="text/javascript">
 $(document).ready(function(){
-  $('.warning').remove();
-  $.get("token.php",function(txt){
-    $(".secure").append('<input type="hidden" name="ts" value="'+txt+'" />');
-  });
-  $("#create").validate({
-    rules: {
-      url: "required",// simple rule, converted to {required:true}
-      url: {
-        url: true
+    $('.warning').remove();
+    $.get("token.php",function(txt){
+      $(".secure").append('<input type="hidden" name="ts" value="'+txt+'" />');
+    });
+    $("#create").validate({
+      rules: {
+        url: "required",// simple rule, converted to {required:true}
+        url: {
+          url: true
+        }
       }
-    }
-  });
+    });
 });
 </script>
-<div id="main">
-<form action="create" id="create" method="post" class="secure">
-  <p class="warning">You must javascript enabled to use this form</p>
-  <p><b>Enter your long url here:</b></p>
-    <input type="text" class="text" name="url" />
-    <input type="submit" class="submit" name="Submit" value="Shorten" />
-</form>
-</div>
 <?php
 $this->load->view('footer');
 ?>
