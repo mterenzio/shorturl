e52df84ea6a5b0b48f6ca27447ac2f00c3251b19
Matt Terenzio <mterenzio@YAR-DIG-M-09.local>
2012-05-24 18:41:22 -0400

    zone set

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -19,6 +19,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
+		$this->load->helper(array('form', 'url'));
+		$this->load->library('form_validation');
 		$this->load->view('welcome_message');
 	}
 }
