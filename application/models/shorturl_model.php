19dc90782c997b5c25452bd5d2ce5fd0220449cc
Matt Terenzio <mterenzio@YAR-DIG-M-09.local>
2012-05-25 18:11:02 -0400

    zone set

--- application/models/shorturl_model.php
+++ application/models/shorturl_model.php
@@ -69,8 +69,10 @@ class Shorturl_model extends CI_Model {
 					'ReturnValues' => "ALL_NEW"
 			));
 			if ($put->isOK()) {
-				print_r($put);  
+				echo "here";
+				echo print_r($put);  
 			} else {
+				echo"false";
 				return false;
 			}
     }
