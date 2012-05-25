5269b7e10ff28db24be0cb40216f001d5bc9f3d3
Matt Terenzio <mterenzio@YAR-DIG-M-09.local>
2012-05-24 23:31:17 -0400

    zone set

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -6,7 +6,7 @@ $this->load->view('header');
       <div class="hero-unit">
         <h1>dig1st!</h1>
         <p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-        <p><a class="btn btn-primary btn-large">Learn more &raquo;</a></p>
+        <p><a class="btn primary large">Learn more &raquo;</a></p>
       </div>
 <?php
 $attributes = array('class' => 'shorturlform', 'id' => 'secure');
