dee931128c1afb4825b539be3ba4a3dfed263f4c
Matt Terenzio <mterenzio@YAR-DIG-M-09.local>
2012-05-24 23:40:01 -0400

    zone set

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -10,7 +10,7 @@ $this->load->view('header');
       </div>
       <!-- Example row of columns -->
       <div class="row">
-        <div class="span4">
+        <div class="span8">
           <h2>Heading</h2>
            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
           <p><a class="btn" href="#">View details &raquo;</a></p>
