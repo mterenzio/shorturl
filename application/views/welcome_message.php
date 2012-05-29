17a47f516ace95c52912a4c295b56df3da6295a6
Matt Terenzio <mterenzio@YAR-DIG-M-09.local>
2012-05-29 10:42:44 -0400

    zone set

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -7,7 +7,8 @@ if (!isset($shorturl)) {
 <div class="container">
 <!-- Main hero unit for a primary marketing message or call to action -->
       <div class="hero-unit">
-        <h1><?php echo $shorturl;?></h1><br />
+        <h1 id="shorty">;
+><?php echo $shorturl;?></h1><br />
 <?php
 $attributes = array('class' => 'shorturlform', 'id' => 'secure');
 echo form_open_multipart('/create', $attributes);
@@ -35,7 +36,7 @@ echo form_close();
         <div class="span16">
           <h2>Install the bookmarklet for easy short url creation . . .</h2>
            <p>Simply drag the button below to the bookmarks bar on your browser and click it when you are on a web page that you'd like to share.</p>
-          <p><a class="btn" href="#">dig1.st</a></p>
+          <p><a class="btn" href="javascript:(function(e,a,g,h,f,c,b,d){if(!(f=e.jQuery)||g>f.fn.jquery||h(f)){c=a.createElement('script');c.type='text/javascript';c.src='http://ajax.googleapis.com/ajax/libs/jquery/'+g+'/jquery.min.js';c.onload=c.onreadystatechange=function(){if(!b&&(!(d=this.readyState)||d=='loaded'||d=='complete')){h((f=e.jQuery).noConflict(1),b=1);f(c).remove()}};a.documentElement.childNodes[0].appendChild(c)}})(window,document,'1.3.2',function($,L){dig1st_script=document.createElement('SCRIPT');dig1st_script.type='text/javascript';dig1st_script.src='http://cdn.dig1.st/js/dig1stbookmarklet.js?x='+(Math.random());document.getElementsByTagName('head')[0].appendChild(dig1st_script);});">dig1.st</a></p>
         </div>
       </div>	  
 <script type="text/javascript">
@@ -44,14 +45,15 @@ $(document).ready(function(){
     $.get("token.php",function(txt){
       $(".shorturlform").append('<input type="hidden" name="ts" value="'+txt+'" />');
     });
-    $("#create").validate({
+    $("#secure").validate({
       rules: {
-        url: "required",// simple rule, converted to {required:true}
-        url: {
+        longurl: "required",// simple rule, converted to {required:true}
+        longurl: {
           url: true
         }
       }
     });
+	$("#shorty").fadeOut("slow").css("background-color", "#FFFF99"
 });
 </script>	  
 <?php
