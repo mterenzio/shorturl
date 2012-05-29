740561278c95ab1fd136b4a41fa2ccdbe0c68287
Matt Terenzio <mterenzio@YAR-DIG-M-09.local>
2012-05-29 13:48:17 -0400

    zone set

--- application/models/shorturl_model.php
+++ application/models/shorturl_model.php
@@ -71,7 +71,7 @@ class Shorturl_model extends CI_Model {
 					'ReturnValues' => "ALL_NEW"
 			));
 			if ($put->isOK()) {
-				return (string) $put->body->longurl->{AmazonDynamoDB::TYPE_STRING}; 
+				echo print_r($put->body->longurl->{AmazonDynamoDB::TYPE_STRING}); 
 			} else {  
 				return false;
 			}
