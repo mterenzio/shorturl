907cfe886759d0f5889cd6d695f1f0db9d861265
Matt Terenzio <mterenzio@YAR-DIG-M-09.local>
2012-06-01 09:14:07 -0400

    zone set

--- application/views/bookmarklet_display_url.php
+++ application/views/bookmarklet_display_url.php
@@ -6,15 +6,15 @@ $this->load->view('basicheader');
                                 text-align:center; 
                                 border:1px solid black; 
                                 background-color:#ccc; 
-                                margin:10px; padding:10px; 
+                                margin:10px; padding:3px; 
 								width: 100px;
                         }
                         #d_clip_button.hover { background-color:#eee; }
                         #d_clip_button.active { background-color:#aaa; }
                 </style>
 
-<div>
-<span id="shorty" style="text-align: center; width: 600px;font-size: 120%;"><?php echo $shorturl;?></span>
+<div style="padding: 10px;">
+<span id="shorty" style="text-align: center; width: 600px;font-size: 150%;"><?php echo $shorturl;?></span>
         
                 <span id="d_clip_button">Copy</span>
 </div>        
