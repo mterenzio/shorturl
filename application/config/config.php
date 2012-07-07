413d24b42e2c8981e1e50f6230633954e835d758
mterenzio <mterenzio@gmail.com>
2012-07-07 16:29:16 -0400

    first try with Twitter auth enabled fixing constants

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = get_cfg_var('aws.secret_key');
+$config['encryption_key'] = 'test';
 
 /*
 |--------------------------------------------------------------------------
