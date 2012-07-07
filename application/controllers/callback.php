4f0b3d8d956bf950adc212b550ff829c4f3d033e
mterenzio <mterenzio@gmail.com>
2012-07-07 19:24:15 -0400

    callback added to routes

--- application/controllers/callback.php
+++ application/controllers/callback.php
@@ -4,8 +4,36 @@ class Callback extends CI_Controller {
 
 	public function index()
 	{		
-		echo "1";	
-
+		$this->load->library('session');
+		require_once(APPPATH.'classes/epicenter/EpiCurl.php');
+		require_once(APPPATH.'classes/epicenter/EpiOAuth.php');
+		require_once(APPPATH.'classes/epicenter/EpiTwitter.php');
+		$toauthkey = get_cfg_var('aws.param4');
+		$toauthksecret = get_cfg_var('aws.param5');
+		$twitterObj = new EpiTwitter($toauthkey, $toauthksecret);	
+		$twitterObj->setToken($_GET['oauth_token']);				
+		try {
+		    $token = $twitterObj->getAccessToken();
+		    $twitterObj->setToken($token->oauth_token, $token->oauth_token_secret);
+		}catch(EpiTwitterException $e){
+		    echo 'We had a problem logging you in to Twitter. It\'s probably their fault. ;)';\
+		    $error = json_decode($e->getMessage());
+		    echo $error;
+		    exit;
+		}catch(Exception $e){
+		    echo "We had a unknown problem logging you in to Twitter. It's probably their fault. ;)";
+		    $error = json_decode($e->getMessage());
+		    echo $error;
+		    exit;
+		}
+		$twitterInfo= $twitterObj->get_accountVerify_credentials();
+		$twitterInfo->response;
+		$this->session->set_userdata('twitter_id', $twitterInfo->id);
+		$this->session->set_userdata('twitter_name', $twitterInfo->screen_name);
+		$this->session->set_userdata('oauth_token', $token->oauth_token);
+		$this->session->set_userdata('oauth_token_secret', $token->oauth_token_secret);
+		//header("Location: ".$_COOKIE["return"]);
+		redirect('/', 'location', 301);
 	}
 			
 }
