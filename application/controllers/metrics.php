9a459c51bdcea2c954eab25bab304e82c0c5964d
mterenzio <mterenzio@gmail.com>
2012-07-08 19:58:25 -0400

    added initial metrics

--- application/controllers/metrics.php
+++ application/controllers/metrics.php
@@ -9,7 +9,7 @@ class Metrics extends CI_Controller {
 			echo "latest stats";
 			$this->load->model('shorturl_model');
 			$items = $this->shorturl_model->getAllByUser($this->session->userdata('twitter_id'));
-			echo $items;
+			echo print_r($items);
 			
 		} else {
 			echo "must be signed in to see metrics";
