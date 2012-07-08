72959ceaa1a820ac1901e768352c0c34faf85e89
mterenzio <mterenzio@gmail.com>
2012-07-08 07:59:55 -0400

    fixes

--- application/models/auth_model.php
+++ application/models/auth_model.php
@@ -3,6 +3,7 @@
 class Auth_model extends CI_Model {
     
 	private $twitterObject;
+	private $twitterInfo;
 	
     public function __construct() {
 		$this->load->library('session');
@@ -26,9 +27,9 @@ class Auth_model extends CI_Model {
 		if ($this->checkAuth()) {
 			$this->twitterObj->setToken($this->session->userdata('oauth_token'), $this->session->userdata('oauth_token_secret'));	
 	    try {
-	    	$twitterInfo = $this->twitterObj->get_accountVerify_credentials();
-	    	$twitterInfo->response;
-			$logon = "Signed in as , ".$twitterInfo->name."<br/>";
+	    	$this->twitterInfo = $this->twitterObj->get_accountVerify_credentials();
+	    	$this->twitterInfo->response;
+			$logon = "Signed in as , ".$this->twitterInfo->name."<br/>";
 	    }catch(EpiTwitterServiceUnavailableException $e){
 	         echo 'Twitter is unavaiable. That stinks but there is nothing we can do.';
 	         exit;
@@ -54,29 +55,3 @@ class Auth_model extends CI_Model {
 }
 
 ?>
-
-
-		if ($this->session->userdata('twitter_id')) {
-				$twitterObj->setToken($this->session->userdata('oauth_token'), $this->session->userdata('oauth_token_secret'));	
-		    try {
-		    	$twitterInfo = $twitterObj->get_accountVerify_credentials();
-		    	$twitterInfo->response;
-				$logon = "Signed in as , ".$twitterInfo->name."<br/>";
-		    }catch(EpiTwitterServiceUnavailableException $e){
-		         echo 'Twitter is unavaiable. That stinks but there is nothing we can do.';
-		         exit;
-		     }catch(Exception $e){
-				 echo $e;
-		         echo "Something unknown is wrong with our connection with twitter. Please try back later.";
-		         exit;
-		    }    		    
-		} else {
-			try {
-				$url = $twitterObj->getAuthorizeUrl();
-				//header("Location: ".$url);
-				$logon = "<a href=\"".$url."\">Sign in with your Twitter account</a><br/>";
-	    	 }catch(Exception $e){
-	         	echo $e;
-	         	exit;
-	    	}  
-		}
