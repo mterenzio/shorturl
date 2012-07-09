59ae8d4075009b3777ec952f5d1d53facbd2a90d
mterenzio <mterenzio@gmail.com>
2012-07-09 09:47:16 -0400

    added initial metrics

--- application/controllers/metrics.php
+++ application/controllers/metrics.php
@@ -10,7 +10,12 @@ class Metrics extends CI_Controller {
 			$this->load->model('shorturl_model');
 			$items = $this->shorturl_model->getAllByUser($this->session->userdata('twitter_id'));
 			foreach ($items as $item)	{
-				echo print_r($item);
+				//echo print_r($item);
+				if (isset($item->count->S)) {
+					echo $item->count->S."<br />";
+				} else {
+					echo "none<br />";
+				}
 				//echo $item->longurl->S."<br />";
 			}		
 		} else {
